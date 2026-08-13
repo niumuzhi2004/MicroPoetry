4668e08a3ea9da86fa12c0df2176fb5fabc94b86
Daniel Niu <daniel.n@wustl.edu>
2026-08-13 21:12:34 +0800

    updated rhyme cache reset

--- hardware/mems/rhyme_cache.sv
+++ hardware/mems/rhyme_cache.sv
@@ -2,8 +2,8 @@ module rhyme_cache #(
     parameter int ADDR_WIDTH = 6,
     parameter int DATA_WIDTH = 12
 ) ( 
-    input logic clk,
-
+    input  logic clk,
+    input  logic rst_n,
     input  logic wr_en,
     input  logic [ADDR_WIDTH-1:0] addr,
     input  logic [DATA_WIDTH-1:0] wr_data,
@@ -13,9 +13,13 @@ module rhyme_cache #(
     logic [DATA_WIDTH-1:0] mem [0:2**ADDR_WIDTH-1];
 
     always_ff @(posedge clk) begin
-        rd_data <= mem[addr];
-        if (wr_en)
-            mem[addr] <= wr_data;
+        if (~rst_n) begin
+            mem[6] <= 0;
+        end else begin
+            rd_data <= mem[addr];
+            if (wr_en)
+                mem[addr] <= wr_data;
+        end
     end
     
 endmodule
